de3744668d86a9384997be4b5252412ba845476c
Clexus <62130504+Clexus@users.noreply.github.com>
2025-05-01 15:04:16 +0800

    Update Main.cs

--- HsMod/Main.cs
+++ HsMod/Main.cs
@@ -9,6 +9,7 @@ namespace HsMod
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
+    
     {
         private void OnGUI()
         {
